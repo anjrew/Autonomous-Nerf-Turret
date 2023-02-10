0e4016246b45e8be613f867feedfe15f35d71304
Andrew Johnson <earyzhe@gmail.com>
2023-02-10 18:54:05 +0100

    Make the first version of the face tracker

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Autonomous-Nerf-Turret
+# Autonomous Nerf Turret
+
 A project dedicated to creating a fully autonomous, AI-powered Nerf gun. The turret is equipped with a camera, allowing it to detect and track human targets. The code includes computer vision algorithms for object detection and tracking. 
+
+# Dependencies
+
+- conda
+- bash
+- [CMake](https://cmake.org/download/)
+
+# Get Started
+
+**Make sure to run the following commands in the project root directory.**
+
+
+- Initialize the environment when first using the project with:
+```bash
+source bin/init.sh
+```
+
+- On each session when coming back and working on the project activate the environment with:
+```bash
+source bin/activate.sh
+```
---
+++ face_tracking.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+import face_recognition
+
+
+
+cap = cv2.VideoCapture(0)
+scaling_factor = 0.5
+
+while True:
+    ret, frame = cap.read()
+   
+    face_locations = face_recognition.face_locations(frame)
+
+
+    for top, right, bottom, left in face_locations:
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        # top *= 4
+        # right *= 4
+        # bottom *= 4
+        # left *= 4
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, "Human", (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        
+    
+    cv2.imshow('Face Detector', frame)
+
+    c = cv2.waitKey(1)
+    ## S 'key'
+    if c == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
