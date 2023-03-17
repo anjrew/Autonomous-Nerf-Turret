2e4f1c611bcf4234c5c04a95f39a11be5b2aec92
Andrew Johnson <earyzhe@gmail.com>
2023-03-17 11:16:46 +0100

    Remove arduino artifacts

--- components/arduino/src/control_via_serial/build/CMakeFiles/turret_control_test.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for turret_control_test.
--- components/arduino/src/control_via_serial/build/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/earyzhe/Desktop/Autonomous-Nerf-Turret/components/stepper_motor/src/control_via_serial/build/_deps/googletest-src
-# Build directory: /Users/earyzhe/Desktop/Autonomous-Nerf-Turret/components/stepper_motor/src/control_via_serial/build/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
